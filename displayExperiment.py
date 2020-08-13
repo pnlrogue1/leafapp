da66e4b6647f78daf62087452aa89f76dce77274
Phil Lane <philipneillane@gmail.com>
2020-08-13 17:59:14 +0100

    Updated get-leaf-info with new battery charge % position

--- displayExperiment.py
+++ displayExperiment.py
@@ -45,7 +45,7 @@ batt_charge_w, batt_charge_h = hanken_bold_font.getsize("Battery Charge:")
 # batt_charge_x = int((inky_display.WIDTH - batt_charge_w) / 2)
 batt_charge_x = 0 + padding
 batt_charge_y = 0 + padding
-draw.text((batt_charge_x, batt_charge_y), "Battery Charge:", inky_display.BLACK, font=hanken_bold_font)
+draw.text((batt_charge_x, batt_charge_y), "Battery Charge:", inky_display.WHITE, font=hanken_bold_font)
 
 # Calculate the positioning and draw the name text
 
@@ -55,7 +55,7 @@ current_charge_w, current_charge_h = hanken_bold_font.getsize(current_charge)
 current_charge_x = int(batt_charge_w + 2)
 # current_charge_y = int(y_top + ((y_bottom - y_top - current_charge_h) / 2))
 current_charge_y = 0 + padding
-draw.text((current_charge_x, current_charge_y), current_charge, inky_display.BLACK, font=hanken_bold_font)
+draw.text((current_charge_x, current_charge_y), current_charge, inky_display.WHITE, font=hanken_bold_font)
 
 # Display the completed name badge
 
