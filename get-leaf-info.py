ec4821cf41a06c29b6aa11cbc8a9edc19e74f316
Phil Lane <philipneillane@gmail.com>
2020-08-13 15:33:25 +0100

    First attempt at outputting battery charge to eink
    Basically just the namebadge example, but with tweaks

--- get-leaf-info.py
+++ get-leaf-info.py
@@ -148,3 +148,68 @@ while leaf_info.answer["BatteryStatusRecords"]["OperationDateAndTime"] == latest
 
 print("latest_date=", latest_date)
 print_info(latest_leaf_info)
+
+# Now for the e-ink display...
+
+inky_display.set_border(inky_display.WHITE)
+
+# Create a new canvas to draw on
+
+img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
+draw = ImageDraw.Draw(img)
+
+# Load the fonts
+
+intuitive_font = ImageFont.truetype(Intuitive, int(22 * scale_size))
+hanken_bold_font = ImageFont.truetype(HankenGroteskBold, int(35 * scale_size))
+hanken_medium_font = ImageFont.truetype(HankenGroteskMedium, int(16 * scale_size))
+
+# Grab the name to be displayed
+
+name = args.name
+
+# Top and bottom y-coordinates for the white strip
+
+y_top = int(inky_display.HEIGHT * (5.0 / 10.0))
+y_bottom = y_top + int(inky_display.HEIGHT * (4.0 / 10.0))
+
+# Draw the red, white, and red strips
+
+#for y in range(0, y_top):
+#    for x in range(0, inky_display.width):
+#        img.putpixel((x, y), inky_display.RED)
+#
+#for y in range(y_top, y_bottom):
+#    for x in range(0, inky_display.width):
+#        img.putpixel((x, y), inky_display.WHITE)
+#
+#for y in range(y_bottom, inky_display.HEIGHT):
+#    for x in range(0, inky_display.width):
+#        img.putpixel((x, y), inky_display.RED)
+
+# Calculate the positioning and draw the "Hello" text
+
+batt_charge_w, batt_charge_h = hanken_bold_font.getsize("Battery Charge:")
+batt_charge_x = int((inky_display.WIDTH - batt_charge_w) / 2)
+batt_charge_y = 0 + padding
+draw.text((batt_charge_x, batt_charge_y), "Battery Charge:", inky_display.WHITE, font=hanken_bold_font)
+
+# Calculate the positioning and draw the "my name is" text
+
+# mynameis_w, mynameis_h = hanken_medium_font.getsize("my name is")
+# mynameis_x = int((inky_display.WIDTH - mynameis_w) / 2)
+# mynameis_y = batt_charge_h + padding
+# draw.text((mynameis_x, mynameis_y), "my name is", inky_display.WHITE, font=hanken_medium_font)
+
+# Calculate the positioning and draw the name text
+
+current_charge = "{}%".format(latest_leaf_info.battery_percent)
+current_charge_w, current_charge_h = intuitive_font.getsize(current_charge)
+current_charge_x = int((inky_display.WIDTH - current_charge_w) / 2)
+current_charge_y = int(y_top + ((y_bottom - y_top - current_charge_h) / 2))
+draw.text((current_charge_x, current_charge_y), current_charge, inky_display.BLACK, font=intuitive_font)
+
+# Display the completed name badge
+
+inky_display.set_image(img)
+inky_display.show()
