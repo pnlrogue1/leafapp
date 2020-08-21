a76a16b7c86f5f0e98fa7a816c2a125d904edda5
Phil Lane <philipneillane@gmail.com>
2020-08-21 16:58:22 +0100

    Add in components to allow output of the date

--- displayExperiment.py
+++ displayExperiment.py
@@ -2,15 +2,20 @@ from PIL import Image, ImageDraw, ImageFont
 from inky import InkyPHAT, InkyWHAT
 from font_hanken_grotesk import HankenGroteskBold, HankenGroteskMedium
 from font_intuitive import Intuitive
+from datetime import datetime
 
 colour = "red"
 inky_display = InkyPHAT(colour)
 scale_size = 1
 padding = 0
+
+# inky_display.set_rotation(180)
+
 inky_display.set_border(inky_display.WHITE)
 
 current_charge_percent = "49"
 current_crange_miles = "74"
+corrected_date = datetime.now()
 # Create a new canvas to draw on
 
 img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
@@ -34,11 +39,11 @@ y_footer_top = inky_display.HEIGHT - 5
 
 if int(current_charge_percent) >= 50:
     charge_range_background = inky_display.WHITE
-    charge_range_text = inky_display.BLACK
+    charge_range_text_colour = inky_display.BLACK
     charge_meter_colour = inky_display.BLACK
 else:
     charge_range_background = inky_display.RED
-    charge_range_text = inky_display.WHITE
+    charge_range_text_colour = inky_display.WHITE
     charge_meter_colour = inky_display.RED
 
 charge_meter_width = round(inky_display.width * (int(current_charge_percent) / 100))
@@ -71,7 +76,7 @@ batt_charge_w, batt_charge_h = hanken_bold_font.getsize("Battery Charge:")
 # batt_charge_x = int((inky_display.WIDTH - batt_charge_w) / 2)
 batt_charge_x = 0 + padding
 batt_charge_y = 0 + padding
-draw.text((batt_charge_x, batt_charge_y), "Battery Charge:", charge_range_text, font=hanken_bold_font)
+draw.text((batt_charge_x, batt_charge_y), "Battery Charge:", charge_range_text_colour, font=hanken_bold_font)
 
 current_charge = "{}%".format(current_charge_percent)
 current_charge_w, current_charge_h = hanken_bold_font.getsize(current_charge)
@@ -79,20 +84,23 @@ current_charge_w, current_charge_h = hanken_bold_font.getsize(current_charge)
 current_charge_x = int(batt_charge_w + 2)
 # current_charge_y = int(battery_charge_bottom + ((y_footer_top - battery_charge_bottom - current_charge_h) / 2))
 current_charge_y = 0 + padding
-draw.text((current_charge_x, current_charge_y), current_charge, charge_range_text, font=hanken_bold_font)
+draw.text((current_charge_x, current_charge_y), current_charge, charge_range_text_colour, font=hanken_bold_font)
 
 # Calculate the positioning and draw the "Available Range" text
 current_range = "{}m".format(current_crange_miles)
 available_range_w, available_range_h = hanken_bold_font.getsize("Available Range:")
 available_range_x = 0 + padding
 available_range_y = 28 + padding
-draw.text((available_range_x, available_range_y), "Available Range:", charge_range_text, font=hanken_bold_font)
+draw.text((available_range_x, available_range_y), "Available Range:", charge_range_text_colour, font=hanken_bold_font)
 
 current_range_w, current_range_h = hanken_bold_font.getsize(current_range)
 current_range_x = int(available_range_w + 2)
 current_range_y = 28 + padding
-draw.text((current_range_x, current_range_y), current_range, charge_range_text, font=hanken_bold_font)
+draw.text((current_range_x, current_range_y), current_range, charge_range_text_colour, font=hanken_bold_font)
 
+datetime_range_x = 0 + padding
+datetime_range_y = range_available_bottom + padding
+draw.text((datetime_range_x, datetime_range_y), corrected_date.strftime("%d %b %I:%M %p"), inky_display.BLACK, font=hanken_bold_font)
 
 # Display the completed image
 
